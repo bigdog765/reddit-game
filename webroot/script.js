8d7a55261eaa85d796187a90e3e973a5369309fa
bigdog765 <aidy.vg@gmail.com>
2024-11-25 21:32:08 -0500

    Add random letter generation

--- webroot/script.js
+++ webroot/script.js
@@ -1,10 +1,13 @@
 class App {
+  letters = []
   constructor() {
     const output = document.querySelector('#messageOutput');
     const increaseButton = document.querySelector('#btn-increase');
     const decreaseButton = document.querySelector('#btn-decrease');
+    const lettersButton = document.querySelector('#btn-letters');
     const usernameLabel = document.querySelector('#username');
     const counterLabel = document.querySelector('#counter');
+    const lettersLabel = document.querySelector('#letters');
     var counter = 0;
 
     // When the Devvit app sends a message with `context.ui.webView.postMessage`, this will be triggered
@@ -42,12 +45,33 @@ class App {
     });
 
     decreaseButton.addEventListener('click', () => {
+      console.log("hi")
       // Sends a message to the Devvit app
       window.parent?.postMessage(
         { type: 'setCounter', data: { newCounter: Number(counter - 1) } },
         '*'
       );
     });
+    lettersButton.addEventListener('click', () => {
+      this.giveInitialLetters();
+      lettersLabel.innerText = this.letters
+    });
+    
+
+  }
+  giveInitialLetters(number=12){
+    this.letters = []
+    const vowels = ['A','E','I','O','U']
+    const randomIndex = Math.floor(Math.random() * number)
+    for(let i = 0; i < number; i++){
+      // Ensure at least one letter is a vowel
+      if (i === randomIndex) this.letters.push(vowels[Math.floor(Math.random() * vowels.length)])
+      else{
+        // Generate a random uppercase letter ASCII value
+        const randomAscii = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        this.letters.push(randomAscii)
+      }
+    }
   }
 }
 
